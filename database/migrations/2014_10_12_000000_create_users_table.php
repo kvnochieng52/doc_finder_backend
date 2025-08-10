69c58104d937673aaa7555bf3b8df3c39475c8bf
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-08-10 08:12:41 +0300

    profile update started

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('is_active')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
