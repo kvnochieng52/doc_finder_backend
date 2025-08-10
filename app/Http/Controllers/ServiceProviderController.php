6f53509bcf2065c63fe39cc27556f22bf2dd5c76
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-08-10 13:33:46 +0300

    profile update started

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class ServiceProviderController extends Controller
 {
